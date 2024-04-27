068679dc448981fb578a6bf684f4cad16cc4993b
Mzzqq <rezarifa661@gmail.com>
2024-04-27 20:46:34 +0700

    feat: add feature to update book data

--- edit-buku.php
+++ edit-buku.php
@@ -3,14 +3,15 @@ include_once("./connect.php");
 
 $id = $_GET["id"];
 
-$query_get_data = mysqli_query("SELECT * FROM buku where id=$id");
+$query_get_data = mysqli_query($db,"SELECT * FROM buku where id=$id");
+$buku = mysqli_fetch_assoc($query_get_data);
 
 if (isset($_POST["submit"])) {
     $nama = $_POST["nama"];
     $isbn = $_POST["isbn"];
     $unit = $_POST["unit"];
 
-    $query = mysqli_query($db, "INSERT INTO buku VALUES (NULL, '$nama', '$isbn', $unit)");
+    $query = mysqli_query($db, "UPDATE buku SET nama='$nama', isbn='$isbn', unit=$unit WHERE id=$id");
 }
 
 
@@ -29,19 +30,21 @@ if (isset($_POST["submit"])) {
 <h1>Form tambah edit buku</h1>
 <form action="" method="POST">
     <label for="nama">Nama</label>
-    <input type="text" id="nama" name="nama">
+    <input value="<?php echo $buku['nama'] ?>" type="text" id="nama" name="nama">
     <br>
     <br>
     <label for="isbn">ISBN</label>
-    <input type="text" id="isbn" name="isbn">
+    <input value="<?php echo $buku['isbn'] ?>" type="text" id="isbn" name="isbn">
     <br>
     <br>
     <label for="unit">Unit</label>
-    <input type="number" id="unit" name="unit">
+    <input value="<?php echo $buku['unit'] ?>" type="number" id="unit" name="unit">
 
     <br>
     <br>
     <button type="submit" name="submit">SUBMIT</button>
 </form>
+<br>
+<a href="./buku.php">Kembali ke halaman buku</a>
 </body>
 </html>
