606094b130aa8456fcfce561a1b6a8cc9698d9c7
Mzzqq <rezarifa661@gmail.com>
2024-04-27 20:11:09 +0700

    feat: add form to input new book information

--- tambah-buku.php
+++ tambah-buku.php
@@ -8,6 +8,22 @@
     <title>FORM TAMBAH BUKU</title>
 </head>
 <body>
+    <h1>Form tambah data buku</h1>
+    <form action="" method="POST">
+        <label for="nama">Nama</label>
+        <input type="text" id="nama" name="nama">
+        <br>
+        <br>
+        <label for="isbn">ISBN</label>
+        <input type="text" id="isbn" name="isbn">
+        <br>
+        <br>
+        <label for="unit">Unit</label>
+        <input type="number" id="unit" name="unit">
 
+        <br>
+        <br>
+        <button type="submit" name="submi">SUBMIT</button>
+    </form>
 </body>
 </html>
