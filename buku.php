0618a06e1d5efec5cda274ab8055ac372cd4ccf8
Mzzqq <rezarifa661@gmail.com>
2024-04-27 19:53:43 +0700

    feat: output the list of books from database

--- buku.php
+++ buku.php
@@ -1,3 +1,9 @@
+<?php
+    include_once("./connect.php");
+
+    $query = mysqli_query($db, "SELECT * FROM buku");
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,8 +12,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Daftar Buku</title>
+
+
 </head>
 <body>
     <h1>Daftar buku</h1>
+
+    <?php foreach ($query as $data) { ?>
+        <?php echo $data["nama"] ?>
+    <?php } ?>
 </body>
 </html>
