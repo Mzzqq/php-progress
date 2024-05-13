4e7304ffaa1ede70ad8dab213940b8392ada3e52
Mzzqq <rezarifa661@gmail.com>
2024-05-13 13:02:15 +0700

    feat: add form for register

---
+++ register.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+    <h2>Register</h2>
+    <form action="./register_process.php" method="post">
+        <label for="email">Email: </label><br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="password">Password: </label><br>
+        <input type="passowrd" id="password" name="password"><br>
+
+        <br>
+        <button type="submit" value="register"></button>
+    </form>
+</body>
+</html>
