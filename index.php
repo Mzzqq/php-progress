106e539e37c4b0295b94a432c16411b56683f6d8
Mzzqq <rezarifa661@gmail.com>
2024-05-13 13:32:20 +0700

    feat: add logout button

--- index.php
+++ index.php
@@ -19,6 +19,9 @@
     <a href="./buku.php">Lihat daftar buku</a>
     <br>
     <a href="./staff.php">Lihat daftar staff</a>
+    <br>
+    <form action="logout_process.php" method="POST">
+    <button type="submit">LOGOUT</button>
+    </form>
 </body>
 </html>
-<?php session_destroy(); ?>
