80afaf84ad73102edbfd91d4884ccb56650ebe1b
Mzzqq <rezarifa661@gmail.com>
2024-05-13 13:18:04 +0700

    feat: add form login

---
+++ login.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Login Admin</h1>
+    <form action="./login_process.php" method="post">
+        <div>
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email">
+        </div>
+        <br>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <br>
+        <button type="submit" value="login">LOGIN</button>
+    </form>
+</body>
+</html>
